f8ffe3c5b7d799c1bbbdabc202a61870374e3c9a
H Brown <hebrown12@gmail.com>
2022-10-28 12:45:52 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,88 @@
+/**
+ * 
+ * ES6
+ * 
+ * var 
+ * 
+ * let 
+ * const 
+ * 
+ * 
+ * functions
+ * 
+ * Declarative function
+ * function functionName() {
+ *  do stuff
+ * }
+ * 
+ * Arrow function
+ * const funcName =(args)=> {
+ *  do stuff
+ * }
+ * 
+ * 
+ * ternaries
+ * 
+ * if (condition) {
+ *  do stuff
+ * } else {
+ *  do something else 
+ * }
+ * 
+ * 
+ * 
+ * condition ? do stuff : do something else 
+ */
+
+// const foo =()=> {
+//     let count = 6;
+//     console.log(count);
+// }
+// foo();
+
+// let baz = 9;
+
+// if (baz == 8) {
+//     console.log(baz);
+// } else {
+//     console.log('This is not right');
+// }
+
+// baz == 8 ? console.log(baz) : null;
+
+const submitBtn = document.getElementById('submitBtn');
+
+let data = {};
+
+const subscribeUser =()=> {
+    const fName = document.getElementById('fName').value;
+    const lName = document.getElementById('lName').value;
+    const email = document.getElementById('email').value;
+    const dob = document.getElementById('dob').value;
+    const year = dob.slice(0, 4);
+    const month = dob.slice(5, 7);
+    const date = dob.slice(8);
+    const planType = document.querySelector('input[name=planType]:checked').value;
+    data.firstName = fName;
+    data.lastName = lName;
+    data.email = email;
+    data.month = month;
+    data.year = year;
+    data.date = date;
+    data.planType = planType;
+
+    console.log(data);
+}
+
+const alertUser =()=>  alert('Please agree to terms and conditons')
+
+
+
+// console.log(submitBtn);
+submitBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    
+    const terms = document.getElementById('terms');
+
+    terms.checked ? subscribeUser() : alertUser();
+})
